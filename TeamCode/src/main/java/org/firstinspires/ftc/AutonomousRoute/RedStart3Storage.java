50e2d0614f79bbdd7b18297338e2850a1f9cad42
Sanne Vos <sannevos99@outlook.com>
2022-03-25 21:14:17 +0100

    Changed red autonomous route (bezier curve)

--- TeamCode/src/main/java/org/firstinspires/ftc/AutonomousRoute/RedStart3Storage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/AutonomousRoute/RedStart3Storage.java
@@ -17,13 +17,14 @@ public class RedStart3Storage extends LinearOpMode
         WedstrijdRobot Robot = new WedstrijdRobot(this);
 
         BezierCurveRoute RedStart3Storage = new BezierCurveRoute(
-                new double[] {-279.63, 743.8875, -597.5, -224.062499999999, 272.459999999998, 13.1450000000009}, //The x-coefficients
-                new double[] {10.755, -53.7750000000001, -1099.4, 2769.4125, -2373.27, 697.2825}, //The y-coefficients
+                new double[] {-326.235, 1041.44250000001, -1045.62500000005, -522.812499999917, 953.609999999949, 92.0149999999993, -263.497499999992}, //The x-coefficients
+                new double[] {12.5475, -75.2849999999999, -1923.94999999999, 6461.96249999997, -8306.44499999995, 4880.97749999996, -1107.16749999999}, //The y-coefficients
                 Robot,
                 0.6,
                 DRIVE_METHOD.STRAFE, //STRAFE or FOLLOW
                 this
         );
+
 
 
         //Initialisation------------------------------------
