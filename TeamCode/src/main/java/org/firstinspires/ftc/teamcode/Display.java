9718bb476f55eb9c004edfe51f1abbbb99e75a97
Craig MacFarlane <sannevos99@outlook.com>
2022-10-07 20:44:07 +0200

    Fully started over and its beautiful!

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Display.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class Display {
+    /** Telemetry instance for displaying data. */
+    private final Telemetry telemetry;
+
+    /** Constructor for the display class. */
+    public Display(Telemetry telemetryArgument) {
+        telemetry = telemetryArgument;
+    }
+
+    /** Shows loading on screen. */
+    public void showLoading(String object) {
+        telemetry.addData("Loading", object);
+        telemetry.update();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotparts/Webcam.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.robotparts;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class Webcam {
+    public Webcam(LinearOpMode opMode) {
+
+    }
+}
