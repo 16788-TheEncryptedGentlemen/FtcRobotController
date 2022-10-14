ee0fbc736da94845feafd91c060e2474aa03d7e6
MysticalMoon <2006123001@sondervick.nl>
2022-10-14 15:57:22 +0200

    WIP

---
+++ TeamCode/src/main/java/drivercontrolled/DriverControlled.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.DriverControlled;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+public class DriverControlled {
+    @TeleOp
+    public class DriverControlled extends OpMode {
+
+    }
+}
