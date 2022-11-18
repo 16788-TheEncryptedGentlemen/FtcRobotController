fd269a9270315855456ab0f88a9327d167181d77
Craig MacFarlane <sannevos99@outlook.com>
2022-11-18 12:06:11 +0100

    Added TODO's and continued working on the webcam.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotparts/Webcam.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotparts/Webcam.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode.robotparts;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.teamcode.webcamgarbage.PipelineTes;
+import org.firstinspires.ftc.teamcode.webcamgarbage.PipelineColourDetection;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
@@ -12,19 +12,19 @@ import org.openftc.easyopencv.OpenCvWebcam;
 public class Webcam {
 
     public OpenCvWebcam webcam;
+    public PipelineColourDetection pipeline;
 
     public Webcam(HardwareMap hardwareMap){
         WebcamName webcam = hardwareMap.get(WebcamName.class, "webcam");
         OpenCvCamera camera = OpenCvCameraFactory.getInstance().createWebcam(webcam);
-
 
         camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
         {
             @Override
             public void onOpened()
             {
-                camera.startStreaming(1920, 1080, OpenCvCameraRotation.UPRIGHT);
-                camera.setPipeline(new PipelineTes());
+                camera.startStreaming(1280, 720, OpenCvCameraRotation.UPRIGHT);
+                camera.setPipeline(pipeline);
 
             }
 
