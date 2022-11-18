d0b524177862a716ad1e43aa418aaeacd2152c7b
Craig MacFarlane <sannevos99@outlook.com>
2022-11-18 12:06:11 +0100

    Added TODO's and continued working on the webcam.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomousroutes/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomousroutes/Test.java
@@ -3,14 +3,25 @@ package org.firstinspires.ftc.teamcode.autonomousroutes;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-import org.firstinspires.ftc.teamcode.autonomousclasses.BezierCurveRoute;
-import org.firstinspires.ftc.teamcode.autonomousclasses.BezierCurveRoute.DRIVE_METHOD;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.robot.CompetitionRobot;
+import org.firstinspires.ftc.teamcode.webcamgarbage.PipelineColourDetection;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvWebcam;
 
 /** Uncomment @Autonomous to show up on the DC controller app */
 @Autonomous
 public class Test extends LinearOpMode
 {
+
+    OpenCvWebcam webcam;
+    PipelineColourDetection pipeline;
+    PipelineColourDetection.SkystonePosition snapshotAnalysis = PipelineColourDetection.SkystonePosition.LEFT; // default
+
+
+    @Override
     public void runOpMode()
     {
 
@@ -18,29 +29,79 @@ public class Test extends LinearOpMode
         //Variables------------------------------------
         CompetitionRobot robot = new CompetitionRobot(this);
 
-        BezierCurveRoute RedStart1Park1 = new BezierCurveRoute(
-                new double[] {-243.780000000001, 290.385000000003, -114.720000000002}, //The x-coefficients
-                new double[] {-22.7049999999988, 44.2149999999988, 43.02}, //The y-coefficients
-                robot,
-                0.6,
-                DRIVE_METHOD.STRAFE, //STRAFE or FOLLOW
-                this
-        );
+        WebcamName webcam = hardwareMap.get(WebcamName.class, "webcam");
+        OpenCvCamera camera = OpenCvCameraFactory.getInstance().createWebcam(webcam);
+        pipeline = new PipelineColourDetection();
 
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                camera.startStreaming(1280, 720, OpenCvCameraRotation.UPRIGHT);
+                camera.setPipeline(pipeline);
 
+            }
 
+            @Override
+            public void onError(int errorCode)
+            {
+                /*
+                 * This will be called if the camera could not be opened
+                 */
+            }
+        });
 
+        while (!isStarted() && !isStopRequested())
+        {
+            telemetry.addData("Realtime analysis", pipeline.getAnalysis());
+            telemetry.update();
 
+            // Don't burn CPU cycles busy-looping in this sample
+            sleep(50);
+        }
 
-        //Initialisation------------------------------------
+        /*
+         * The START command just came in: snapshot the current analysis now
+         * for later use. We must do this because the analysis will continue
+         * to change as the camera view changes once the robot starts moving!
+         */
+        snapshotAnalysis = pipeline.getAnalysis();
 
+        /*
+         * Show that snapshot on the telemetry
+         */
+        telemetry.addData("Snapshot post-START analysis", snapshotAnalysis);
+        telemetry.update();
 
-        waitForStart();
+        switch (snapshotAnalysis)
+        {
+            case LEFT:
+            {
+                /* Your autonomous code */
+                break;
+            }
 
-        //Run program------------------------------------
-        //Run program------------------------------------
+            case RIGHT:
+            {
+                /* Your autonomous code */
+                break;
+            }
 
-        RedStart1Park1.executeWithPointSkip();
+            case CENTER:
+            {
+                /* Your autonomous code*/
+                break;
+            }
+        }
+
+        /* You wouldn't have this in your autonomous, this is just to prevent the sample from ending */
+        while (opModeIsActive())
+        {
+            // Don't burn CPU cycles busy-looping in this sample
+            sleep(50);
+        }
+
 
     }
 
