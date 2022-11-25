c44424624c729fca479bd9b632af28257d2ac53a
Craig MacFarlane <sannevos99@outlook.com>
2022-11-25 15:16:02 +0100

    Made more new autonomous programs without the webcam and changed a mistake in two of the previous routes.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomousroutes/BlueStart1Park2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomousroutes/BlueStart1Park2.java
@@ -15,8 +15,8 @@ public class BlueStart1Park2 extends LinearOpMode {
         CompetitionRobot Robot = new CompetitionRobot(this);
 
         BezierCurveRoute BlueStart1Park2 = new BezierCurveRoute(
-                new double[] {-243.780000000001, 290.385000000003, -114.720000000002}, //The x-coefficients
-                new double[] {-22.7049999999988, 44.2149999999988, 43.02}, //The y-coefficients
+                new double[] {370.45, -856.416666666667, 884.3, -254.933333333333, -143.001666666667}, //The x-coefficients
+                new double[] {-3.9833333333338, -51.7833333333265, 1422.04999999998, -2039.46666666665, 799.853333333328}, //The y-coefficients
                 Robot,
                 0.6,
                 DRIVE_METHOD.STRAFE, //STRAFE or FOLLOW
