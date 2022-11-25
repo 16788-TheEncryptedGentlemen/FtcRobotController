5d8c0dc5bcb0b167716a900f08a18d8f191ed0d0
MysticalMoon <2006123001@sondervick.nl>
2022-11-25 18:04:11 +0100

    Added new lift functions. The lift still needs to be tested.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotparts/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotparts/Lift.java
@@ -1,10 +1,12 @@
 package org.firstinspires.ftc.teamcode.robotparts;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 // TODO: Make sure this program works!!!! Make different height for the different junctions.
 // TODO: Aditi: Add fullstops to documentation and make sure spaces are correct!!!
+// TODO: Test It!
 
 public class Lift {
 
@@ -13,27 +15,43 @@ public class Lift {
     /** The right DCMotor of the lift */
     public DcMotorEx right;
 
-    //TODO: Aditi: change names for conficureration so that the first letter is a lowercase letter.
+    //TODO: Aditi: change names for configureration so that the first letter is a lowercase letter.
 
     /** The lift has a right and left DCMotor */
     public Lift(HardwareMap hardwareMap)
     {
         left = hardwareMap.get(DcMotorEx.class, "LeftLift");
+        left.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         right = hardwareMap.get(DcMotorEx.class, "RightLift");
+        right.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
-    /** The robot lift closes */
+    /** The robot lift goes up */
     public void up()
     {
         left.setPower(1);
         right.setPower(-1);
     }
-    /** The robot lift open */
+
+    public void autonomousliftup()
+    {
+        left.setTargetPosition(5);
+        right.setTargetPosition(5);
+    }
+
+    /** The robot lift down */
     public void down()
     {
         left.setPower(-1);
         right.setPower(1);
     }
+
+    public void autonomousliftdown()
+    {
+        left.setTargetPosition(5);
+        right.setTargetPosition(5);
+    }
+
     /** The robot lift stops. */
     public void stop()
     {
