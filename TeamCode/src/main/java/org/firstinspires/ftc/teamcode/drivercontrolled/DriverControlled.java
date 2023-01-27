0f02b87b04233e5724230398fcf8b8f53a0c45c6
Craig MacFarlane <sannevos99@outlook.com>
2023-01-27 15:20:45 +0100

    Sorted function loop into new functions.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivercontrolled/DriverControlled.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivercontrolled/DriverControlled.java
@@ -6,7 +6,6 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.robot.CompetitionRobot;
 
 
-
 @TeleOp
 public class DriverControlled extends OpMode {
 
@@ -35,7 +34,7 @@ public class DriverControlled extends OpMode {
     private double TurnSpeed = 0;
 
     /** The angle that the robot needs to correct to whilst strafing with gyro correction. */
-    private double GyroCorrectionAngle = 0;
+    private final double GyroCorrectionAngle = 0;
     /** The deviation of the robot whilst strafing with gyro correction. */
     private double DeviationAngle = 0;
     /** The correction factor for the motors whilst strafing with gyro correction. */
@@ -45,9 +44,7 @@ public class DriverControlled extends OpMode {
 
     //--------------------------------------------------------------------
     @Override
-    /** Initialisation */
-    public void init()
-    {
+    /** Initialisation */ public void init() {
         robot = new CompetitionRobot(this);
     }
     //--------------------------------------------------------------------
@@ -60,7 +57,7 @@ public class DriverControlled extends OpMode {
         // * Getting inputs from controller, imu and calculating variables:
         // * LeftJoyY, LeftJoyX, RobotAngle, StrafeSpeed, StrafeAngle, TurnSpeed
         //--------------------------------------------------------------------
-       /** Left joystick up and down on the gamepad */
+        /** Left joystick up and down on the gamepad */
         LeftJoyY = -gamepad1.left_stick_y;
         /** Left joystick left and right on the gamepad */
         LeftJoyX = gamepad1.left_stick_x;
@@ -68,28 +65,9 @@ public class DriverControlled extends OpMode {
         /** The angle the robot is in with help of the IMU.*/
         RobotAngle = robot.imu.getAngle();
 
-        /** The speed of strafing.*/
-        StrafeSpeed = Math.sqrt(Math.pow(LeftJoyX,2) + Math.pow(LeftJoyY,2));
 
-        if(StrafeSpeed != 0 && LeftJoyX != 0)
-            StrafeAngle = Math.signum(LeftJoyX)*Math.toDegrees(Math.acos(LeftJoyY/StrafeSpeed));
-        else if(LeftJoyY < 0)
-            StrafeAngle = 180;
-        else
-            StrafeAngle = 0;
-
-        //The control stick is not perfect and it can have a radius bigger than 1. We fix that here.
-        if(StrafeSpeed > 1)
-            StrafeSpeed = 1;
-
-        /** The speed of turning is controlled by moving the right joystick horizontally. */
-        TurnSpeed = gamepad1.right_stick_x;
-
-        robot.drivetrain.setStrafeValues(StrafeAngle, StrafeSpeed);
-        robot.drivetrain.addSpeed(-TurnSpeed, -TurnSpeed, TurnSpeed, TurnSpeed);
         //--------------------------------------------------------------------
-        if(gamepad1.start)
-            robot.imu.reset();
+        if (gamepad1.start) robot.imu.reset();
 
 
         telemetry.addData("X", robot.odometry.getX());
@@ -99,28 +77,10 @@ public class DriverControlled extends OpMode {
         telemetry.addData("encoder", robot.lift.left.getCurrentPosition());
 
         //--------------------------------------------------------------------
-        /** Controls of the grabber on the robot for the beacon. */
-        if (gamepad2.x) {
-            telemetry.addLine("Grab");
-            robot.grabber.grab();
-        } else if (gamepad2.b) {
-            telemetry.addLine("Drop");
-            robot.grabber.drop();
-        }
+        controlGrabber();
 
         //--------------------------------------------------------------------
-        /** Controls of the lift on the robot. */
-        telemetry.addData("Left stick y:", gamepad2.left_stick_y);
-        if (gamepad2.left_stick_y > 0.1) {
-            telemetry.addLine("Up");
-            robot.lift.up();
-        } else if (gamepad2.left_stick_y < -0.1) {
-            telemetry.addLine("Down");
-            robot.lift.down();
-        } else  {
-            telemetry.addLine("StopLift");
-            robot.lift.stop();
-        }
+        controlLift();
 
 
         //--------------------------------------------------------------------
@@ -133,42 +93,85 @@ public class DriverControlled extends OpMode {
         //--------------------------------------------------------------------
 
 
+        //--------------------------------------------------------------------
+        //Gyro correction
+        //--------------------------------------------------------------------
+
+        controlDrivetrain();
+
+    }
+
+    private void controlDrivetrain() {
+        /** The speed of strafing.*/
+        StrafeSpeed = Math.sqrt(Math.pow(LeftJoyX, 2) + Math.pow(LeftJoyY, 2));
+
+        if (StrafeSpeed != 0 && LeftJoyX != 0)
+            StrafeAngle = Math.signum(LeftJoyX) * Math.toDegrees(Math.acos(LeftJoyY / StrafeSpeed));
+        else if (LeftJoyY < 0) StrafeAngle = 180;
+        else StrafeAngle = 0;
+
+        //The control stick is not perfect and it can have a radius bigger than 1. We fix that here.
+        if (StrafeSpeed > 1) StrafeSpeed = 1;
+
+        /** The speed of turning is controlled by moving the right joystick horizontally. */
+        TurnSpeed = gamepad1.right_stick_x;
+
+        robot.drivetrain.setStrafeValues(StrafeAngle, StrafeSpeed);
+        robot.drivetrain.addSpeed(-TurnSpeed, -TurnSpeed, TurnSpeed, TurnSpeed);
+
         // Place AntiJerkTimer and GyroCorrection here, if there is an error.
 
         DeviationAngle = RobotAngle - GyroCorrectionAngle;
         // Place DeviationAngle code here, if there is an error.
 
-            if(DeviationAngle > -30 && DeviationAngle < 30){
-                CorrectionFactor = DeviationAngle/30;
-            }
-            else{
-                CorrectionFactor = Math.signum(DeviationAngle);
-            }
-            //TODO: deze moet eigenlijk wel iets doen
-           // Robot.drivetrain.addSpeed(CorrectionFactor,CorrectionFactor,-CorrectionFactor,-CorrectionFactor);
+        if (DeviationAngle > -30 && DeviationAngle < 30) {
+            CorrectionFactor = DeviationAngle / 30;
+        } else {
+            CorrectionFactor = Math.signum(DeviationAngle);
+        }
+        //TODO: deze moet eigenlijk wel iets doen
+        // Robot.drivetrain.addSpeed(CorrectionFactor,CorrectionFactor,-CorrectionFactor,-CorrectionFactor);
 
-        //--------------------------------------------------------------------
-        //Gyro correction
-        //--------------------------------------------------------------------
 
         robot.drivetrain.fixMotorSpeedOverflow();
 
 
-
-        if(gamepad1.right_bumper)
-            robot.drivetrain.multiplySpeed(0.7);
-        else if(gamepad1.left_bumper)
-            robot.drivetrain.multiplySpeed(0.2);
-        else
-            robot.drivetrain.multiplySpeed(0.5);
+        if (gamepad1.right_bumper) robot.drivetrain.multiplySpeed(0.7);
+        else if (gamepad1.left_bumper) robot.drivetrain.multiplySpeed(0.2);
+        else robot.drivetrain.multiplySpeed(0.5);
 
         robot.drivetrain.setPower();
 
         telemetry.addData("DeviationAngle", DeviationAngle);
         telemetry.addData("controllery", LeftJoyY);
-        telemetry.addData("GyroCorrectionFactor",CorrectionFactor);
+        telemetry.addData("GyroCorrectionFactor", CorrectionFactor);
         telemetry.addData("Heading", robot.drivetrain.imu.getAngle());
+    }
 
+    private void controlGrabber() {
+        /** Controls of the grabber on the robot for the beacon. */
+        if (gamepad2.x) {
+            telemetry.addLine("Grab");
+            robot.grabber.grab();
+        } else if (gamepad2.b) {
+            telemetry.addLine("Drop");
+            robot.grabber.drop();
+        }
+    }
+
+    private void controlLift() {
+        /** Controls of the lift on the robot. */
+        telemetry.addData("Left stick y:", gamepad2.left_stick_y);
+        if (gamepad2.left_stick_y > 0.1) {
+            telemetry.addLine("Up");
+            robot.lift.up();
+        } else if (gamepad2.left_stick_y < -0.1) {
+            telemetry.addLine("Down");
+            robot.lift.down();
+        } else {
+            telemetry.addLine("StopLift");
+            robot.lift.stop();
+        }
     }
 
     //--------------------------------------------------------------------
