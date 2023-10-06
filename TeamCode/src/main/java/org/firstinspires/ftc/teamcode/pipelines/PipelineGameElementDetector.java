2c79b9aad7353718e897982d5da3a4a3b714f65d
Craig MacFarlane <sannevos99@outlook.com>
2023-10-06 15:23:27 +0200

    WIP Started working on the webcam. 1 error. Created the webcam,robot,display and pipeline class.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pipelines/PipelineGameElementDetector.java
@@ -0,0 +1,88 @@
+package org.firstinspires.ftc.teamcode.webcamgarbage;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+public class PipelineGameElementDetector extends OpenCvPipeline {
+
+    public enum ConeColour {UNKNOWN, RED, BLUE};
+    private static final int CONE_CORRECTION_RED = 14000;
+    private static final int CONE_CORRECTION_BLUE = 1000;
+    private static final int YELLOW_CONE_PIXELS = 1000;
+    private static final int RED_CONE_PIXELS = 7000;
+    private static final int BLUE_CONE_PIXELS = 20;
+    public ConeColour cone = ConeColour.UNKNOWN;
+    /*
+     * Working variables
+     */
+    Mat hsvImage = new Mat();
+    Mat crop = new Mat();
+    Mat redMat = new Mat();
+    Mat blueMat = new Mat();
+    Mat yellowMat = new Mat();
+
+    Rect frame = new Rect(300,100,400,600);
+
+    int redCount = 0;
+    int blueCount = 0;
+    int yellowCount = 0;
+
+    int result = 1;
+
+
+    // Making scalars, the limits of the colour that the robot thinks of as red, yellow or blue.
+    // Degrees are divided by 2 since OpenCV 0 to 180 degrees.
+    //  TODO: figure out why the second and third number is either 255 or 100.
+    Scalar redMin = new Scalar(170,100,100);
+    Scalar redMax = new Scalar(180,255,255);
+    Scalar yellowMin = new Scalar(20,100,100);
+    Scalar yellowMax = new Scalar(30,255,255);
+    Scalar blueMin = new Scalar(180 / 2.0,67 * 2.55,50 * 2.55);
+    Scalar blueMax = new Scalar(280 / 2.0,255,255);
+
+    int[] out = {0,0,0};
+
+
+    // the running funtcion
+    @Override
+    public Mat processFrame(Mat input) {
+
+        crop = new Mat(input, frame);
+        Imgproc.cvtColor(crop, hsvImage, Imgproc.COLOR_RGB2HSV);
+
+
+        // check frame colors within color min max values, store those pixels in colorMat
+        Core.inRange(hsvImage, redMin, redMax, redMat);
+        Core.inRange(hsvImage, yellowMin, yellowMax, yellowMat);
+        Core.inRange(hsvImage, blueMin, blueMax, blueMat);
+
+        // count number of pixels of each color and correct for cone colour.
+        redCount = Core.countNonZero(redMat) - (cone == ConeColour.RED ? CONE_CORRECTION_RED : 0);
+        yellowCount = Core.countNonZero(yellowMat);
+        blueCount = Core.countNonZero(blueMat) - (cone == ConeColour.BLUE ? CONE_CORRECTION_BLUE : 0);
+
+        out[0] = redCount;
+        out[1] = yellowCount;
+        out[2] = blueCount;
+
+        if (yellowCount > YELLOW_CONE_PIXELS ) {
+            result = 2;
+            return crop;
+        }
+
+        if (redCount > RED_CONE_PIXELS ){
+            result = 1;
+            return crop;
+        }
+//
+        else {
+            result = 3;
+            return crop;
+        }
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotparts/Webcam.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode.robotparts;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.pipelines.PipelineGameElementDetector;
+import org.firstinspires.ftc.teamcode.webcamgarbage.PipelineColourDetection;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+public class Webcam {
+
+    public PipelineGameElementDetector pipeline;
+
+    public Webcam(HardwareMap hardwareMap) {
+        WebcamName webcam = hardwareMap.get(WebcamName.class, "Webcam");
+        OpenCvCamera camera = OpenCvCameraFactory.getInstance().createWebcam(webcam);
+
+        pipeline = new PipelineGameElementDetector();
+
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                camera.startStreaming(1280, 720, OpenCvCameraRotation.UPRIGHT);
+                camera.setPipeline(pipeline);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+                /*
+                 * This will be called if the camera could not be opened
+                 */
+            }
+        });
+    }
+
+    public int[] getAnalysis(){
+        return pipeline.getAnalysis();
+    }
+
+    public int getResult(){
+        return pipeline.getResult();
+    }
+
+
+
+}
