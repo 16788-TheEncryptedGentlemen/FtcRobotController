d46b1e7c724bba9b3488890bf7dd6ec64cdbfe47
MysticalMoon <2006123001@sondervick.nl>
2023-10-18 11:59:45 +0200

    Added new classes for Grabber and Arm. WIP with the arm

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotparts/Arm.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.robotparts;
+
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+
+public class Arm {
+
+    public DcMotorEx Arm;
+
+
+    public Arm (HardwareMap hardwareMap)
+    {
+        Arm = hardwareMap.get(DcMotorEx.class, "Arm");
+    }
+
+    public void MoveArmUp()
+    {
+        Arm.setPower(0.9);
+
+    }
+    public void MoveArmDown()
+    {
+        Arm.setPower(-0.9);
+    }
+    public void StopArm()
+    {
+        Arm.setPower(0.0);
+
+    }
+
+}
