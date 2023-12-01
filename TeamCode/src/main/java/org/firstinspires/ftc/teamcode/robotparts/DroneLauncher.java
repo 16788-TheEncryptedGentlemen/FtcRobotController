a57760e1510fb8696e08c8c69bba0963d1ec1f53
MysticalMoon <2006123001@sondervick.nl>
2023-12-01 17:10:48 +0100

    Remade drone launcher and fine-tuned arm and grabber.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotparts/DroneLauncher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotparts/DroneLauncher.java
@@ -1,32 +1,23 @@
 package org.firstinspires.ftc.teamcode.robotparts;
 
-import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 
 public class DroneLauncher {
 
-    public Servo droneLauncherGrabber;
-    public DcMotorEx droneLauncherArm;
+    public Servo servo;
 
     public DroneLauncher(HardwareMap hardwareMap) {
-        droneLauncherGrabber = hardwareMap.get(Servo.class, "droneLauncher");
-        droneLauncherArm = hardwareMap.get(DcMotorEx.class,"droneLauncher");
+        servo = hardwareMap.get(Servo.class, "droneLauncher");
     }
 
-    public void MoveBackwards() {
-        droneLauncherGrabber.setPosition(0.0);
-        droneLauncherArm.setPower(-0.5);
+    public void launch() {
+        servo.setPosition(0.6);
     }
 
-    public void Release() {
-        droneLauncherGrabber.setPosition(0.5);
-        droneLauncherArm.setPower(0.0);
+    public void reverse () {
+        servo.setPosition(1.0);
     }
 
-    public void Stop() {
-        droneLauncherGrabber.setPosition(0.0);
-        droneLauncherArm.setPower(0.0);
-    }
 }
