906dde6dfcfce35659d78f84c1592b76a0dd9fb3
Craig MacFarlane <sannevos99@outlook.com>
2023-12-20 15:42:09 +0100

    The camera works.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomousroutes/AutonoomTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomousroutes/AutonoomTest.java
@@ -15,17 +15,21 @@ public class AutonoomTest extends LinearOpMode {
         //Variables
         CompetitionRobot robot = new CompetitionRobot(this);
 //        robot.webcam.pipeline.cone = PipelineRedMarkerDetection.ConeColour.RED;
-        double output;
+        int output = 1;
+        int output2 = 1;
 
         while (!isStarted() && !isStopRequested())
         {
             // debugging
 
 
-            output = robot.webcam.getResult();
+            output = robot.webcam.pipeline.getBluePosition();
+
+            output2 = robot.webcam.pipeline.getRedPosition();
             // result
 //            result = robot.webcam.getResult();
-            telemetry.addData("eenOfAnderGetal", output);
+            telemetry.addData("bluePosition", output);
+            telemetry.addData("redPosition", output2);
             telemetry.update();
 
         }
@@ -39,6 +43,22 @@ public class AutonoomTest extends LinearOpMode {
 
         /** Initialisation. */
         waitForStart();
+
+
+        switch(output) {
+            case 0: // LEFT
+                robot.drivetrain.turnRobotAO(-20);
+                sleep(500);
+                robot.drivetrain.driveStraight(20, 0.4);
+                break;
+            case 2: // RIGHT
+                robot.drivetrain.turnRobotAO(20);
+                sleep(500);
+                robot.drivetrain.driveStraight(20, 0.4);
+                break;
+            default: // Default MIDDLE
+                robot.drivetrain.driveStraight(20, 0.4);
+        }
 
 
 
