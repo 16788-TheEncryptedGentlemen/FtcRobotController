019116eb69534ecdc2ff76aac9baa85e102d22b4
Craig MacFarlane <sannevos99@outlook.com>
2023-12-22 12:16:11 +0100

    The camera works.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomousroutes/AutonoomTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomousroutes/AutonoomTest.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.autonomousroutes;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.teamcode.autonomousclasses.BezierCurveRoute;
 import org.firstinspires.ftc.teamcode.robots.CompetitionRobot;
 
 /** Comment to make the program disappear from the driverstation app. */
@@ -17,6 +18,25 @@ public class AutonoomTest extends LinearOpMode {
 //        robot.webcam.pipeline.cone = PipelineRedMarkerDetection.ConeColour.RED;
         int output = 1;
         int output2 = 1;
+
+        BezierCurveRoute RedStart1Case0 = new BezierCurveRoute(
+                new double[] {-111.135, 136.23, -53.3766666666667}, //The x-coefficients
+                new double[] {-3.58499999999987, 59.7499999999999, 5.97500000000001}, //The y-coefficients
+                robot,
+                0.4,
+                BezierCurveRoute.DRIVE_METHOD.STRAFE, //STRAFE or FOLLOW
+                this
+        );
+
+        BezierCurveRoute RedStart1Case2 = new BezierCurveRoute(
+                new double[] {100.38, -142.205, 67.7166666666666}, //The x-coefficients
+                new double[] {-13.145, 74.0900000000001, 4.3816666666666}, //The y-coefficients
+                robot,
+                0.4,
+                BezierCurveRoute.DRIVE_METHOD.STRAFE, //STRAFE or FOLLOW
+                this
+        );
+
 
         while (!isStarted() && !isStopRequested())
         {
@@ -47,17 +67,16 @@ public class AutonoomTest extends LinearOpMode {
 
         switch(output) {
             case 0: // LEFT
-                robot.drivetrain.turnRobotAO(-20);
-                sleep(500);
-                robot.drivetrain.driveStraight(20, 0.4);
+                RedStart1Case0.executeWithPointSkip();
+                robot.drivetrain.driveStraight(-10, 0.4);
                 break;
             case 2: // RIGHT
-                robot.drivetrain.turnRobotAO(20);
-                sleep(500);
-                robot.drivetrain.driveStraight(20, 0.4);
+                RedStart1Case2.executeWithPointSkip();
+                robot.drivetrain.driveStraight(-10, 0.4);
                 break;
             default: // Default MIDDLE
-                robot.drivetrain.driveStraight(20, 0.4);
+                robot.drivetrain.driveStraight(100, 0.4);
+                robot.drivetrain.driveStraight(-25, 0.4);
         }
 
 
