895fad21121add488a8e7eafcc35e471fd9063e7
MysticalMoon <2006123001@sondervick.nl>
2023-12-28 16:28:56 +0100

    Adjusted grabber positions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotparts/Grabber.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotparts/Grabber.java
@@ -23,14 +23,14 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
         /** The robot grabber closes. */
         public void grab()
         {
-            left.setPosition(0);
-            right.setPosition(1);
+            left.setPosition(0.2);
+            right.setPosition(0.8);
         }
         /** The robot grabber opens. */
         public void drop()
         {
-            left.setPosition(0.6);
-            right.setPosition(0.2);
+            left.setPosition(0.4);
+            right.setPosition(0.4);
         }
         /** The robot grabber stops. */
         public void stop()
