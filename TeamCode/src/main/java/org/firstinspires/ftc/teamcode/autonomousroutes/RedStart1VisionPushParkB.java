984512be9248a3a4f152875ddefc125d68d641dd
Craig MacFarlane <sannevos99@outlook.com>
2024-01-02 11:37:47 +0100

    Theoretically finished this autonomous program but it still has to be tested and perfected.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomousroutes/RedStart1VisionPushParkB.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomousroutes/RedStart1VisionPushParkB.java
@@ -13,6 +13,9 @@ public class RedStart1VisionPushParkB extends LinearOpMode {
     private final boolean SKIP_VISION = false;
     private BezierCurveRoute case0;
     private BezierCurveRoute case2;
+    private BezierCurveRoute case0ParkB;
+    private BezierCurveRoute case1ParkB;
+    private BezierCurveRoute case2ParkB;
     private CompetitionRobot robot;
 
     private void initAutonomous() {
@@ -30,6 +33,33 @@ public class RedStart1VisionPushParkB extends LinearOpMode {
         case2 = new BezierCurveRoute(
                 new double[]{100.38, -142.205, 67.7166666666666}, //The x-coefficients
                 new double[]{-13.145, 74.0900000000001, 4.3816666666666}, //The y-coefficients
+                robot,
+                0.4,
+                BezierCurveRoute.DRIVE_METHOD.STRAFE, //STRAFE or FOLLOW
+                this
+        );
+
+        case0ParkB= new BezierCurveRoute(
+                new double[] {-272.46, 215.100000000001, 187.216666666665, 98.5875000000008, -709.83, 415.063333333333}, //The x-coefficients
+                new double[] {46.6049999999964, -492.937499999991, 1400.14166666666, -1315.99375000001, 826.940000000008, -217.490000000003}, //The y-coefficients
+                robot,
+                0.4,
+                BezierCurveRoute.DRIVE_METHOD.STRAFE, //STRAFE or FOLLOW
+                this
+        );
+
+        case1ParkB = new BezierCurveRoute(
+                new double[]{100.38, -142.205, 67.7166666666666}, //The x-coefficients
+                new double[]{-13.145, 74.0900000000001, 4.3816666666666}, //The y-coefficients
+                robot,
+                0.4,
+                BezierCurveRoute.DRIVE_METHOD.STRAFE, //STRAFE or FOLLOW
+                this
+        );
+
+        case2ParkB = new BezierCurveRoute(
+                new double[] {-243.780000000001, 290.385000000003, -114.720000000002}, //The x-coefficients
+                new double[] {-22.7049999999988, 44.2149999999988, 43.02}, //The y-coefficients
                 robot,
                 0.4,
                 BezierCurveRoute.DRIVE_METHOD.STRAFE, //STRAFE or FOLLOW
@@ -72,17 +102,29 @@ public class RedStart1VisionPushParkB extends LinearOpMode {
     }
 
     private void middlePixelPlacement() {
+        //Push pixel naar de middelste streep.
         robot.drivetrain.driveStraight(80, 0.4);
+        //Rij een stuk naar achter zodat de pixel niet meer onder de robot ligt.
         robot.drivetrain.driveStraight(-25, 0.4);
+        //Rij naar de backstage en parkeer.
+        case1ParkB.executeWithPointSkip();
     }
 
     private void rightPixelPlacement() {
+        //Push pixel naar de rechter streep.
         case2.executeWithPointSkip();
+        //Rij een stuk naar achter zodat de pixel niet meer onder de robot ligt.
         robot.drivetrain.driveStraight(-10, 0.4);
+        //Rij naar de backstage en parkeer.
+        case2ParkB.executeWithPointSkip();
     }
 
     private void leftPixelPlacement() {
+        //Push pixel naar de linker streep.
         case0.executeWithPointSkip();
+        //Rij een stuk naar achter zodat de pixel niet meer onder de robot ligt.
         robot.drivetrain.driveStraight(-10, 0.4);
+        //Rij naar de backstage en parkeer.
+        case0ParkB.executeWithPointSkip();
     }
 }
