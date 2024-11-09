80c4f288e145dab051798fff58350ebdf570e621
ftc <ftc@sondervick.nl>
2024-11-09 11:06:18 +0100

    Added new autonomous route for parking

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomousroutes/JustParkingObservationZone.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.autonomousroutes;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.robots.CompetitionRobot;
+
+/** Comment to make the program disappear from the driverstation app. */
+@Autonomous
+public class JustParkingObservationZone extends LinearOpMode {
+    CompetitionRobot robot;
+
+    final double POWER = 0.5;
+
+    public void runOpMode() {
+        // Initialisation.
+        robot = new CompetitionRobot(this);
+        waitForStart();
+
+        robot.drivetrain.driveStraight(90, POWER);
+
+        // todo: checken of in ieder geval éen deel van éen wiel erin staat.
+    }
+}
