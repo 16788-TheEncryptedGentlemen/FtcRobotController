dd3d9eb124d700c153cdd836db54e32a28f46074
ftc <ftc@sondervick.nl>
2025-01-10 16:20:31 +0100

    Code for Triangle Robot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/TriangleRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/TriangleRobot.java
@@ -3,12 +3,15 @@ package org.firstinspires.ftc.teamcode.robots;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 import org.firstinspires.ftc.teamcode.robotparts.TriangleDrivetrain;
+import org.firstinspires.ftc.teamcode.robotparts.TriangleDrivetrainSnel;
 
 public class TriangleRobot {
     public TriangleDrivetrain drivetrain;
+    public TriangleDrivetrainSnel drivetrainsnel;
 
     public TriangleRobot(OpMode runningOpMode) {
         drivetrain = new TriangleDrivetrain(runningOpMode);
+        drivetrainsnel = new TriangleDrivetrainSnel(runningOpMode);
     }
 
  }
